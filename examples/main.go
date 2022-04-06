71841d740aff17cb98d4a75ad82bdb404fea67b0
yug-elastic <yug.rajani@elastic.co>
2022-04-06 16:32:58 +0530

    Update the example to resolve linting errors

--- examples/main.go
+++ examples/main.go
@@ -9,9 +9,15 @@ import (
 func Example() {
 	out := make(chan bay.TriggerEvent)
 	b := bay.Bayeux{}
-	creds := bay.GetSalesforceCredentials()
+	var ap bay.AuthenticationParameters
+	ap.ClientID = "3MVG9pRsdbjsbdjfm1I.fz3f7zBuH4xdKCJcM9B5XLgxXh2AFTmQmr8JMn1vsadjsadjjsadakd_C"
+	ap.ClientSecret = "E9FE118633BC7SGDADUHUE81F19C1D4529D09CB7231754AD2F2CA668400619"
+	ap.Username = "salesforce.user@email.com"
+	ap.Password = "foobar"
+	ap.TokenURL = "https://login.salesforce.com/services/oauth2/token"
+	creds, _ := bay.GetSalesforceCredentials(ap)
 	replay := "-1"
-	c := b.Channel(out, replay, creds, "channel")
+	c := b.Channel(out, replay, *creds, "channel")
 	for {
 		select {
 		case e := <-c:
